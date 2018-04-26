0964ea6424ac5a3bcdc5e8a2b9e84e4571a7475a
StanMarche <chaypas@hotmail.fr>
2018-04-26 14:07:22 +0200

    update 0.0.1

--- app/src/main/java/com/example/remyl/interstellar/MainActivity.java
+++ app/src/main/java/com/example/remyl/interstellar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.remyl.interstellar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
-
-
+    public void pictureClick(View v)
+    {
+        Intent intent = new Intent(this, ImageActivity.class);
+        startActivity(intent);
 
     }
+
+
 }
